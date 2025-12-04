344227eadac11a82fd1a3aa6beb659c396b3c0ce
Morteza Hosseini <smhroot@gmail.com>
2025-12-04 21:53:46 +0100

    loading chess boarding and implementing turn.

---
+++ src/Chess.zig
@@ -0,0 +1,222 @@
+const rl = @import("raylib");
+const std = @import("std");
+const print = std.debug.print;
+
+pub const Piece = enum(u8) {
+    None,
+    WPawn,
+    WKnight,
+    WBishop,
+    WRook,
+    WQueen,
+    WKing,
+    BPawn,
+    BKnight,
+    BBishop,
+    BRook,
+    BQueen,
+    BKing,
+};
+
+pub const Chess = struct {
+    pub const PieceSize: f32 = 60;
+
+    selected_piece: ?rl.Vector2 = null,
+    board: [8][8]Piece = undefined,
+    texture: rl.Texture2D = undefined,
+    board_position: rl.Vector2 = .{ .x = 0, .y = 0 },
+    //turn : white move, !turn : black move
+    turn: bool = true,
+    // square_size: f32 = 60,
+
+    pub fn init(allocator: std.mem.Allocator) !*Chess {
+        const c = try allocator.create(Chess);
+        c.* = .{};
+        try c.setup();
+        return c;
+    }
+
+    fn setup(c: *Chess) !void {
+        setupBoard(c);
+        const screenWidth = PieceSize * 8;
+        const screenHeight = PieceSize * 8;
+        rl.initWindow(screenWidth, screenHeight, "Chess");
+        rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+        c.texture = try rl.loadTexture("assets/pieces.png");
+        if (c.texture.id <= 0) {
+            print("Failed to load texture: {}\n", .{c.texture.id});
+            std.process.exit(1);
+        } else {
+            print("Texture loaded successfully: ({},{})\n", .{ c.texture.width, c.texture.height });
+        }
+    }
+
+    pub fn deinit(c: *Chess, allocator: std.mem.Allocator) void {
+        rl.unloadTexture(c.texture);
+        allocator.destroy(c);
+    }
+
+    pub fn run(c: *Chess) void {
+        defer rl.closeWindow(); // Close window and OpenGL context
+        while (!rl.windowShouldClose()) { // Detect window close button or ESC key
+            // logic of the game and change piece location
+            c.checkMouse();
+            rl.beginDrawing();
+            defer rl.endDrawing();
+            rl.clearBackground(.black);
+            // draw logic
+            c.draw();
+        }
+    }
+
+    fn draw(c: *Chess) void {
+        c.drawBoard();
+    }
+
+    fn movePiece(c: *Chess, from: rl.Vector2, to: rl.Vector2) void {
+        c.turn = !c.turn;
+        const piece = c.board[@intFromFloat(from.x)][@intFromFloat(from.y)];
+        c.board[@intFromFloat(from.x)][@intFromFloat(from.y)] = .None;
+        c.board[@intFromFloat(to.x)][@intFromFloat(to.y)] = piece;
+    }
+
+    fn checkMouse(c: *Chess) void {
+        const pressed = rl.isMouseButtonPressed(.left);
+        // const hold = rl.isMouseButtonDown(.left);
+        // if (!hold or !pressed) return;
+        if (!pressed) return;
+        const mouse_pos = rl.getMousePosition();
+        const pos = mouse_pos.subtract(c.board_position);
+        if (pos.x < 0 or pos.y < 0 or pos.x >= 8 * PieceSize or pos.y >= 8 * PieceSize) {
+            c.selected_piece = null;
+            return;
+        }
+        const piece_x = @floor(pos.x / PieceSize);
+        const piece_y = @floor(pos.y / PieceSize);
+        const to_piece = c.board[@intFromFloat(piece_y)][@intFromFloat(piece_x)];
+        if (c.selected_piece) |from| {
+            const from_piece = c.board[@intFromFloat(from.x)][@intFromFloat(from.y)];
+            const t = @intFromEnum(to_piece);
+            const f = @intFromEnum(from_piece);
+            const wmove = c.turn and f < 7 and (t == 0 or t > 6);
+            const bmove = !c.turn and f > 6 and t < 7;
+            if (wmove or bmove) c.movePiece(from, .{ .x = piece_y, .y = piece_x });
+            c.selected_piece = null;
+        } else if (to_piece != .None) c.selected_piece = .{ .x = piece_y, .y = piece_x };
+    }
+
+    fn drawBoard(c: *Chess) void {
+        const startx = c.board_position.x;
+        const starty = c.board_position.y;
+        for (0..8) |i| {
+            for (0..8) |j| {
+                const colorw = rl.Color{ .r = 230, .g = 230, .b = 230, .a = 255 };
+                const colorb = rl.Color{ .r = 70, .g = 70, .b = 70, .a = 255 };
+                const color = if ((i + j) % 2 == 0) colorw else colorb;
+                const x: i32 = @intFromFloat(startx + @as(f32, @floatFromInt(j)) * PieceSize);
+                const y: i32 = @intFromFloat(starty + @as(f32, @floatFromInt(i)) * PieceSize);
+                rl.drawRectangle(x, y, PieceSize, PieceSize, color);
+            }
+        }
+        if (c.selected_piece) |selected_piece| {
+            const selectedx: i32 = @intFromFloat(startx + selected_piece.y * PieceSize);
+            const selectedy: i32 = @intFromFloat(starty + selected_piece.x * PieceSize);
+            rl.drawRectangleLines(selectedx, selectedy, PieceSize, PieceSize, rl.Color{ .r = 0, .g = 255, .b = 0, .a = 255 });
+        }
+        for (0..8) |i| {
+            for (0..8) |j| {
+                if (c.board[i][j] == .None) continue;
+                var source_rect = rl.Rectangle{
+                    .x = 0,
+                    .y = 0,
+                    .width = PieceSize,
+                    .height = PieceSize,
+                };
+                switch (c.board[i][j]) {
+                    .WPawn => {
+                        source_rect.x = 5 * PieceSize;
+                        source_rect.y = PieceSize;
+                    },
+                    .WKing => {
+                        source_rect.x = PieceSize;
+                        source_rect.y = PieceSize;
+                    },
+                    .WQueen => {
+                        source_rect.y = PieceSize;
+                    },
+                    .WRook => {
+                        source_rect.x = 2 * PieceSize;
+                        source_rect.y = PieceSize;
+                    },
+                    .WBishop => {
+                        source_rect.x = 4 * PieceSize;
+                        source_rect.y = PieceSize;
+                    },
+                    .WKnight => {
+                        source_rect.x = 3 * PieceSize;
+                        source_rect.y = PieceSize;
+                    },
+                    .BPawn => {
+                        source_rect.x = 5 * PieceSize;
+                    },
+                    .BKing => {
+                        source_rect.x = PieceSize;
+                    },
+                    .BQueen => {},
+                    .BRook => {
+                        source_rect.x = 2 * PieceSize;
+                    },
+                    .BBishop => {
+                        source_rect.x = 4 * PieceSize;
+                    },
+                    .BKnight => {
+                        source_rect.x = 3 * PieceSize;
+                    },
+                    .None => {
+                        unreachable;
+                    },
+                }
+                const dest_rect = rl.Rectangle{
+                    .x = startx + @as(f32, @floatFromInt(j)) * PieceSize,
+                    .y = starty + @as(f32, @floatFromInt(i)) * PieceSize,
+                    .width = PieceSize,
+                    .height = PieceSize,
+                };
+                rl.drawTexturePro(c.texture, source_rect, dest_rect, .{ .x = 0, .y = 0 }, 0, .white);
+            }
+        }
+    }
+
+    /// origin is top left.
+    /// so bottom right is (7, 7)
+    /// and top right is (0, 7)
+    fn setupBoard(c: *Chess) void {
+        c.board[0][0] = .BRook;
+        c.board[0][1] = .BKnight;
+        c.board[0][2] = .BBishop;
+        c.board[0][3] = .BQueen;
+        c.board[0][4] = .BKing;
+        c.board[0][5] = .BBishop;
+        c.board[0][6] = .BKnight;
+        c.board[0][7] = .BRook;
+        for (0..8) |i| {
+            c.board[1][i] = .BPawn;
+        }
+        c.board[7][0] = .WRook;
+        c.board[7][1] = .WKnight;
+        c.board[7][2] = .WBishop;
+        c.board[7][3] = .WQueen;
+        c.board[7][4] = .WKing;
+        c.board[7][5] = .WBishop;
+        c.board[7][6] = .WKnight;
+        c.board[7][7] = .WRook;
+        for (0..8) |i| {
+            c.board[6][i] = .WPawn;
+        }
+        for (2..6) |i| {
+            for (0..8) |j| {
+                c.board[i][j] = .None;
+            }
+        }
+    }
+};
---
+++ src/main.zig
@@ -0,0 +1,14 @@
+const rl = @import("raylib");
+const std = @import("std");
+const C = @import("chess.zig").Chess;
+const print = std.debug.print;
+
+pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    const game = try C.init(allocator);
+    game.run();
+    game.deinit(allocator);
+    const leak = gpa.deinit(); // Checks for leaks in debug
+    print("\nLeaks:\n {}", .{leak});
+}
